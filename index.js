8217f115e27740bdcb034c2f8475cf8c99e86ff7
samuelsuu <107080477+samuelsuu@users.noreply.github.com>
2023-12-20 11:51:20 +0100

    update

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 function highlightDivisor() {
   var userInput = document.getElementById("number").value;
 
-  document.getElementById("output").innerHTML= "";
+  // Clear previous content
+  document.getElementById("output").innerHTML = "";
 
-  for (var i = 1; i<=9; i++){
-    if(userInput % i === 0){
-      var span = document.createElement("span");
-      span.textContent = i;
-      document.getElementById("output").appendChild(span);
+  // Check if the input is not empty
+  if (userInput !== "") {
+    for (var i = 1; i <= 1000; i++) {
+      if (userInput % i === 0) {
+        var span = document.createElement("span");
+        span.textContent = i;
+        document.getElementById("output").appendChild(span);
+      }
     }
   }
 }
